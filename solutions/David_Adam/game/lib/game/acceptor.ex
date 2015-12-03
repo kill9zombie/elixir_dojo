4096f9ae0f126ed6efe40b224a14ea68fd1d0499
Adam Pridmore <adam_pridmore@hotmail.com>
2015-12-03 19:44:20 +0000

    Added Adam & David's simple MUD

---
+++ solutions/David_Adam/game/lib/game/acceptor.ex
@@ -0,0 +1,72 @@
+defmodule Game.Acceptor do
+  require Logger
+  alias Game.Player, as: Player
+  alias Game.Board, as: Board
+
+  #
+  # The main Elixir documentation reference is here:
+  #  http://elixir-lang.org/docs.html
+  # 
+  # .. just watch the version that you're using.
+  #
+  # There's a really good into to Elixir too (just longer than this dojo)
+  #
+  # http://elixir-lang.org/getting-started/introduction.html
+  #
+  #
+ 
+  # This is our entry point, it's called by the Listener when they add us to the Task Supervisor.
+  def start(socket) do
+    # From here, we'll just display the intoduction screen to the user, then go into the main loop.
+    intro(socket) |> loop
+  end
+
+  # This is just the intro or splash banner that the user sees.
+  # The String or IO.ANSI modules can be handy.
+  defp greeting do
+    IO.ANSI.cyan <> "\x{232c} Welcome to Leeds Code Dojo! \x{232c}\r\n\r\n" <> IO.ANSI.default_color
+  end
+
+  # This is the introduction, where we show the banner and register the user.
+  defp intro(socket) do
+    :gen_tcp.send(socket, greeting())
+    :gen_tcp.send(socket, "player name: ")
+
+    # If the socket is in raw mode, we can select how many bytes to read.
+    # In our case it isn't, 0 just means 'all bytes'.
+    # http://www.erlang.org/doc/man/gen_tcp.html#recv-2
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    Logger.debug fn -> "#{__MODULE__} received data: #{inspect data}" end
+
+    player_name = String.strip(data)
+    Logger.debug fn -> "#{__MODULE__} player name: #{inspect player_name}" end
+    # Register the player, this will either add a new player or
+    # return their existing record.  See the help with `h Game.Player.regiser`.
+    Player.register(player_name)
+
+    # The last term is returned by the function.
+    {player_name, socket}
+  end
+
+  # This is the main loop.
+  defp loop({player_name, socket}) do
+    # Get the player's position on the board and display
+    # the text to the user.
+    {:ok, position} = Player.position(player_name)
+    {:ok, {room_text}} = Board.room(position)
+    :gen_tcp.send(socket, "#{room_text}\r\n")
+    :gen_tcp.send(socket, "\r\n")
+
+    # Now we get a command from the user and deal with the result.
+    :gen_tcp.send(socket, "command> ")
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    Logger.debug fn -> "#{__MODULE__} received data: #{inspect data}" end
+
+    case String.strip(data) do
+      "north" -> Game.Player.move(player_name, :north)
+                 loop({player_name, socket})
+      "quit" -> :gen_tcp.send(socket, "goodbye\r\n")
+    end
+  end
+
+end
---
+++ solutions/David_Adam/game/lib/game/board.ex
@@ -0,0 +1,65 @@
+defmodule Game.Board do
+  require Logger
+
+  @doc """
+  Starts a new game.
+  """
+  def start_link do
+    Agent.start_link(fn -> newboard end, name: __MODULE__)
+  end
+
+  @doc """
+  Setup a new board/course.
+  """
+  def newboard do
+    [
+      [{"Edge of the forest."}, {"The cliffs of Aaaarrrrggh!"} ,{"Almscliff Crag."}, {"Edge of the forest."} ],
+      [{"Edge of the forest."}, {"Mr Foleys"} ,{"The Dojo, Hai!"}, {"Edge of the forest."} ],
+      [{"Edge of the forest."}, {"Town Hall."} ,{"Deserted Hospital."}, {"Edge of the forest."} ],
+      [{"Edge of the forest."}, {"Welcome to the forest, please go north to start your journey."} ,{"nothing here yet!"}, {"Edge of the forest."} ],
+      [{"Edge of the forest."}, {"Edge of the forest."} ,{"Edge of the forest."}, {"Edge of the forest."} ]
+    ]
+  end
+
+  @doc"We want the start position for a new player"
+  def start_position do
+    # {row, column}
+    {3,1}
+  end
+
+  @doc ~S"""
+  Find the text for a given row and column on the board.
+
+  Returns {:ok, {text}} if everything's ok, or {:error, :bad_position} if you've gone out of bounds.
+
+  Example (We want row 3, column 1):
+      iex> Game.Board.room({3,1})
+      {:ok, {"Welcome to the forest, please go north to start your journey."}}
+      iex>
+
+  The board looks like this:
+
+        North
+
+       0 1 2 3 ...
+      0
+      1
+      2
+      3
+      .
+      .
+        South
+
+  """
+  def room({row, _column}) when row < 0, do: {:error, :bad_position}
+  def room({_row, column}) when column < 0, do: {:error, :bad_position}
+  def room({row, column}) do
+    Logger.debug fn -> "#{__MODULE__} Fetching row: #{inspect row} column: #{inspect column}" end
+    Agent.get(__MODULE__, fn(board) ->
+      board |> Enum.at(row,[]) |>  Enum.at(column)
+    end) |> do_room
+  end
+  defp do_room(nil), do: {:error, :bad_position}
+  defp do_room(term), do: {:ok, term}
+
+end
